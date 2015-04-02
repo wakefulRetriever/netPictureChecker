6bc2667b474f7e7860df8ebe8f05ec3d96bebaa5
storm <2090624557@qq.com>
2015-04-02 16:39:26 +0800

    time 2

--- app/src/main/java/com/storm/netpicturechecker/MainActivity.java
+++ app/src/main/java/com/storm/netpicturechecker/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("TEST","TEST");
-
+        Log.d("TEST","TEST");
+        Log.d("TEST","TEST");
+        Log.d("TEST","TEST");
     }
 
 
